e28426c003011cc3f13a5ffa2ae58ef3360d2951
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-04-21 13:17:08 -0300

    Corrigiendo rutas para levantar correctamente desde electron.

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'pilas-engine',
     environment: environment,
-    rootURL: '/',
+    rootURL: '',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
