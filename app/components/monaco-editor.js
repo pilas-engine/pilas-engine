fa9c238ddbabb92021056f5fb2f683fbbe9668f3
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-05-18 11:52:01 -0300

    Reparando monaco-editor para que funcione sobre electron.

---
+++ app/components/monaco-editor.js
@@ -0,0 +1,103 @@
+import Ember from 'ember';
+import layout from 'ember-monaco-editor/templates/components/monaco-editor';
+import getFrameById from 'ember-monaco-editor/utils/get-frame-by-id';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: ['monaco-editor'],
+  code: '// demo',
+  loading: true,
+
+  init () {
+    this._super(...arguments);
+
+    const subscription = event => {
+      // Ignore messages not coming from this iframe
+      if (event.source === this.get('frame') && event.data && event.data.updatedCode) {
+        if (this.attrs.onChange) {
+          this.attrs.onChange(event.data.updatedCode);
+        }
+      }
+
+      if (event.source === this.get('frame') && event.data && event.data.message) {
+        if (event.data.message === "load-complete") {
+          this.onLoadEditor(this.get('frame').editor);
+        }
+      }
+    };
+
+    this.set('_subscription', subscription);
+    window.addEventListener('message', subscription);
+  },
+
+  didInsertElement () {
+    const frame = getFrameById(this.get('elementId'));
+    const frameDoc = frame.document;
+    this.set('frame', frame);
+
+    frameDoc.open();
+    frameDoc.write(`
+
+
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id="print-modal-content">
+      <head>
+        <script src="vs/loader.js"></script>
+        <script>
+          window.require.config({ paths: { 'vs': 'vs' }});
+
+          window.require(['vs/editor/editor.main'], function () {
+            if (typeof monaco !== "undefined") {
+              var editor = monaco.editor.create(document.getElementById('monaco-editor-wrapper'), {
+                language: 'typescript',
+              });
+
+              /*
+              , {
+                value: '${this.get('code')}',
+                language: '${this.get('language')}'
+              });
+              */
+
+              var origin = window.location.origin;
+
+              // TODO: when the code is autocompleted we don't get this even firing
+              // For example type a single ', the editor will autocomplete '' we only get
+              // the first ', not ''
+
+              editor.onDidChangeModelContent(function (event) {
+                window.top.postMessage({updatedCode: editor.getValue()}, origin);
+              });
+
+              window.top.postMessage({message: "load-complete"}, origin);
+              window.editor = editor;
+
+            }
+          });
+          </script>
+      </head>
+      <body>
+        <div id="monaco-editor-wrapper" style="width:800px;height:600px;border:1px solid grey"></div>
+      </body>
+      </html>
+
+      `);
+    frameDoc.close();
+
+  },
+
+  onLoadEditor(editor) {
+    window.editor = editor;
+
+    if (this.get('code')) {
+      editor.setValue(this.get('code'));
+    }
+
+    this.set('loading', false);
+  },
+
+  willDestroyElement () {
+    this._super(...arguments);
+    window.removeEventListener('message', this.get('_subscription'));
+  }
+});
