bae315ddb0719388fdce1d5aacd4a840a65bc70b
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-05-27 17:38:03 -0300

    Modificando scripts de travis para usar chrome en lugar de firefox.

--- testem.js
+++ testem.js
@@ -4,10 +4,9 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "Firefox"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "Firefox",
     "Chrome"
   ]
 };
