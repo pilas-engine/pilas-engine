ccfee6c0693986cfb4a7bd8be5760dacfca45e28
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-09-27 13:03:33 -0300

    Realizando prueba de gameloop usando un arbol de entidades.

--- app/components/pilas-phaser-experimental.js
+++ app/components/pilas-phaser-experimental.js
@@ -2,35 +2,336 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   codigo: `
-    var playState = {
+    var debugState = {
+      sprites: {},
+      entidades: [],
+
         init: function () {
-            //Se llama cuando se inicia el estado
         },
+
         preload: function () {
-            //Se cargan Imagenes y archivos de recurso
         },
+
         create: function () {
-            //Se crea el personaje, los enemigos, los sonidos, el fondo del juego, etc
+          this.graphics = this.game.add.graphics(0, 0);
+          game.stage.backgroundColor = "fbb";
         },
+
         update: function () {
-            //Logica del Juego como los movimientos, las colisiones, el movimiento del personaje, etc
+          this.graphics.clear();
+          this.game.world.bringToTop(this.graphics);
+
+          // Actualiza las entidades
+          this.entidades.map((e) => {
+
+            if (!this.sprites[e.id]) {
+              console.log("Creando sprite cache para " + e.id, e);
+              this.sprites[e.id] = this.game.add.sprite(e.x, e.y, e.imagen);
+            }
+
+            sprite = this.sprites[e.id];
+            sprite.pivot.x = e.centro_x;
+            sprite.pivot.y = e.centro_y;
+            sprite.x = e.x;
+            sprite.y = e.y;
+
+            this._dibujar_punto_de_control(e.x, e.y);
+          });
+
+          this.game.debug.text("Hay " + this.entidades.length + " entidades", 10, 20);
+
         },
+
+        _dibujar_punto_de_control: function(x, y) {
+          let d = 5;
+          let d2 = 4;
+
+          this.graphics.lineStyle(4, 0x000000);
+          this.graphics.moveTo(x-d, y-d);
+          this.graphics.lineTo(x+d, y+d);
+
+          this.graphics.moveTo(x+d, y-d);
+          this.graphics.lineTo(x-d, y+d);
+
+          this.graphics.lineStyle(2, 0xFFFFFF);
+          this.graphics.moveTo(x-d2, y-d2);
+          this.graphics.lineTo(x+d2, y+d2);
+
+          this.graphics.moveTo(x+d2, y-d2);
+          this.graphics.lineTo(x-d2, y+d2);
+        },
+    };
+
+    var playState = {
+
+        // cache de sprites
+        sprites: {
+        },
+
+        habilidades: {
+          'seguir puntero': function(e) {
+            e.x = e.x + 1;
+
+            if (e.x > 600) {
+              e.x = -100;
+            }
+          }
+        },
+
+        // lista de entidades
+        entidades: [
+          {
+            id: 123,
+            nombre: 'pepe',
+            x: 100,
+            y: 100,
+            imagen: 'ember',
+            eliminado: false,
+            centro_x: 0,
+            centro_y: 0,
+            habilidades: [{
+              nombre: 'seguir puntero'
+            }]
+          }
+        ],
+
+        init: function () {
+        },
+
+        preload: function () {
+        },
+
+        create: function () {
+          this.graphics = this.game.add.graphics(0, 0);
+        },
+
+        update: function () {
+          this.graphics.clear();
+          this.game.world.bringToTop(this.graphics);
+          guardar();
+
+          // Elimina entidades marcadas para eliminar.
+          this.entidades = this.entidades.filter((e) => {
+
+            if (e.eliminado) {
+              if (this.sprites[e.id]) {
+                this.sprites[e.id].destroy();
+                delete this.sprites[e.id];
+              }
+
+              return false;
+            }
+
+            return true;
+          });
+
+          // Actualiza las entidades
+          this.entidades.map((e) => {
+
+            if (!this.sprites[e.id]) {
+              console.log("Creando sprite cache para " + e.id, e);
+              this.sprites[e.id] = this.game.add.sprite(e.x, e.y, e.imagen);
+
+              if (e.fisica) {
+                this.game.physics.p2.enable(this.sprites[e.id], true);
+                this.sprites[e.id].body.restitution = 1;
+
+                if (e.fisica_circulo) {
+                  this.sprites[e.id].body.setCircle(e.fisica_radio || 30);
+                }
+              }
+            }
+
+            if (e.habilidades) {
+              e.habilidades.map((h) => {
+                this.habilidades[h.nombre](e);
+              });
+            }
+
+            sprite = this.sprites[e.id];
+
+            if (sprite.body) {
+              sprite.body.static = e.estatico;
+            }
+
+            if (sprite.body) {
+              if (sprite.body.static) {
+                sprite.body.x = e.x;
+                sprite.body.y = e.y;
+              } else {
+                e.x = sprite.body.x;
+                e.y = sprite.body.y;
+              }
+            } else {
+              sprite.x = e.x;
+              sprite.y = e.y;
+            }
+
+            sprite.pivot.x = e.centro_x;
+            sprite.pivot.y = e.centro_y;
+
+            this._dibujar_punto_de_control(e.x, e.y);
+          });
+
+          this.game.debug.text("Hay " + this.entidades.length + " entidades", 10, 20);
+
+        },
+
+        _dibujar_punto_de_control: function(x, y) {
+          let d = 5;
+          let d2 = 4;
+
+          this.graphics.lineStyle(4, 0x000000);
+          this.graphics.moveTo(x-d, y-d);
+          this.graphics.lineTo(x+d, y+d);
+
+          this.graphics.moveTo(x+d, y-d);
+          this.graphics.lineTo(x-d, y+d);
+
+          this.graphics.lineStyle(2, 0xFFFFFF);
+          this.graphics.moveTo(x-d2, y-d2);
+          this.graphics.lineTo(x+d2, y+d2);
+
+          this.graphics.moveTo(x+d2, y-d2);
+          this.graphics.lineTo(x-d2, y+d2);
+        },
+
         render: function () {
-            //Depurar lo que se renderiza
-            //this.game.debug.body(this.player);
         },
+
         pause: function () {
             //Cuando el juego es pausado
         },
+
         shutdown: function () {
             //Cuando se sale del estado
         }
     };
 
-    var game = new Phaser.Game(320, 240, Phaser.AUTO, 'game');
-    game.state.add('play', playState);
-    game.state.start('play');
-    
+    function preload() {
+      game.load.image('ember', 'imagenes/ember.png');
+      game.load.image('pelota', 'imagenes/pelota.png');
+    }
+
+
+    historia = [];
+
+    // Toma un snapshot de la escena
+    function guardar() {
+      imagenGuardada = JSON.parse(JSON.stringify(playState.entidades));
+      historia.push(imagenGuardada);
+      //console.log("Clonando entidades: " + historia.length);
+    }
+
+    // Restaura la última imagen guardada
+    function restaurar() {
+      if (historia.length < 1) {
+        console.warn("No quedan snapshots para restaurar");
+      } else {
+
+        // Si está en la simulación real, cambia a la escena maquina del tiempo
+        if (game.state.current === "play") {
+          game.state.start('maquina del tiempo');
+        }
+
+        debugState.entidades = historia.pop()
+        console.log("restaurando entidades, quedan " + historia.length);
+      }
+    }
+
+
+    function create() {
+      game.stage.backgroundColor = "bbb";
+
+
+      // Aplica límites al escenario para las simulaciones físicas.
+      game.world.setBounds(0, 0, 300, 300);
+      game.physics.startSystem(Phaser.Physics.P2JS);
+      game.physics.p2.gravity.y = 400;
+      game.physics.p2.restitution = 0.9;
+
+      // evita que phaser detenga la escena si pierde el foco de la ventana.
+      this.stage.disableVisibilityChange = true;
+
+      // Carga la escena principal
+      game.state.add('play', playState);
+      game.state.start('play');
+
+      // carga la máquina del tiempo
+      game.state.add('maquina del tiempo', debugState);
+
+      p = crear("pelota");
+      p.imagen = "pelota";
+      p.fisica = true;
+      p.estatico = true;
+      p.y = 310;
+
+      p.habilidades.push({
+        nombre: 'seguir puntero'
+      })
+
+      p = crear("pelota");
+      p.imagen = "pelota";
+      p.fisica = true;
+
+      p = crear("pelota");
+      p.imagen = "pelota";
+      p.fisica = true;
+
+      p.fisica_circulo = true;
+      p.fisica_radio = 25;
+      p.x = 100;
+    }
+
+
+    // Inicialización
+
+    var game = new Phaser.Game(500, 500, Phaser.AUTO, 'game', {
+        preload: preload,
+        create: create
+      }
+    );
+
+    function crear(nombre) {
+      let id = Math.round(Math.random() * 10000000);
+      let entidad = {
+            id: nombre + "_" + id,
+            nombre: nombre,
+            x: 50,
+            y: 50,
+            imagen: 'ember',
+            eliminado: false,
+            centro_x: 0,
+            centro_y: 0,
+            fisica: false,
+            estatico: false,
+            habilidades: [],
+      };
+
+      playState.entidades.push(entidad);
+      return entidad;
+    }
+
+    function obtener(nombre) {
+      items = playState.entidades.filter(e => e.nombre === nombre);
+
+      if (items) {
+        return items[0];
+      } else {
+        throw new Error("No existe una entidad con el nombre: " + nombre);
+      }
+    }
+
+    let contexto = {
+      game: game,
+      obtener: obtener,
+      crear: crear,
+      playState: playState,
+      guardar: guardar,
+      restaurar: restaurar,
+    }
+
+    contexto;
 `,
 
   didInsertElement() {
@@ -38,7 +339,14 @@ export default Ember.Component.extend({
     let codigo = this.get("codigo");
 
     iframe.onload = () => {
-      iframe.contentWindow.eval(codigo);
+      let contexto = iframe.contentWindow.eval(codigo);
+
+      window.obtener = contexto.obtener;
+      window.game = contexto.game;
+      window.crear = contexto.crear;
+      window.playState = contexto.playState;
+      window.guardar = contexto.guardar;
+      window.restaurar = contexto.restaurar;
     };
   }
 });
