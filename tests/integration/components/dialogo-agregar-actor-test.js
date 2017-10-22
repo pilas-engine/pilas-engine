20ea08aa0b994125df88551ee74b63eee6cb1be0
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-10-22 01:13:06 -0300

    Reparando tests.

--- tests/integration/components/dialogo-agregar-actor-test.js
+++ tests/integration/components/dialogo-agregar-actor-test.js
@@ -1,25 +1,16 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('dialogo-agregar-actor', 'Integration | Component | dialogo agregar actor', {
-  integration: true
-});
+moduleForComponent(
+  "dialogo-agregar-actor",
+  "Integration | Component | dialogo agregar actor",
+  {
+    integration: true
+  }
+);
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{dialogo-agregar-actor}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#dialogo-agregar-actor}}
-      template block text
-    {{/dialogo-agregar-actor}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test("it renders", function(assert) {
+  this.set("agregarUnActor", () => {});
+  this.render(hbs`{{dialogo-agregar-actor agregarUnActor=agregarUnActor}}`);
+  assert.ok(this.$().text());
 });
