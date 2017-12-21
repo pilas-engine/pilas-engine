1d7ed80c3947b6571e1debd3ad8a686c0a5de29e
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-12-21 00:39:12 -0300

    Creando queryparams para serializar la escena y moviendo la creación del proyecto al controlador del editor.

--- app/controllers/editor.js
+++ app/controllers/editor.js
@@ -1,19 +1,77 @@
 import Ember from "ember";
 import json_a_string from "../utils/json-a-string";
 import string_a_json from "../utils/string-a-json";
+import QueryParams from "ember-parachute";
 
-export default Ember.Controller.extend({
-  serializado: null,
+const queryParams = new QueryParams({
+  serializado: {
+    defaultValue: null,
+    refresh: true,
+    replace: true
+  }
+});
+
+export default Ember.Controller.extend(queryParams.Mixin, {
+  proyecto: null,
+
+  setup(event) {
+    let params = event.queryParams;
+
+    if (params.serializado) {
+      this.cargarProyectoDesdeQueryParams(params);
+    } else {
+      let proyecto = this.crearProyectoInicial();
+      this.set("proyecto", proyecto);
+    }
+  },
+
+  cargarProyectoDesdeQueryParams(params) {
+    let proyecto = string_a_json(params.serializado);
+    let proyectoComoObjetoEmber = Ember.Object.create(proyecto);
+
+    proyectoComoObjetoEmber.escenas = proyecto.escenas.map(escena => {
+      escena.actores = escena.actores.map(a => Ember.Object.create(a));
+      return Ember.Object.create(escena);
+    });
+
+    this.set("proyecto", proyectoComoObjetoEmber);
+  },
+
+  crearProyectoInicial() {
+    return Ember.Object.create({
+      titulo: "Proyecto demo",
+      escenas: [
+        Ember.Object.create({
+          nombre: "escena principal",
+          id: 1,
+          actores: []
+        }),
+        Ember.Object.create({
+          nombre: "Otra escena",
+          id: 2,
+          actores: []
+        })
+      ]
+    });
+  },
+
+  reset({ queryParams }, isExiting) {
+    if (isExiting) {
+      this.resetQueryParams();
+    }
+  },
 
   actions: {
     serializar(model) {
       let str = json_a_string(model);
+      this.set("serializado", str);
       string_a_json(str);
     },
     agregarEscena(model) {
       model.escenas.pushObject({
         nombre: "demo",
-        id: 3
+        id: 3,
+        actores: []
       });
     },
     agregarActor(model) {
