cfa524551a27c46154b5e90523730bb34aba37d8
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-01-08 14:04:41 -0300

    Reparando tests

--- tests/unit/controllers/acercade-test.js
+++ tests/unit/controllers/acercade-test.js
@@ -1,12 +1,10 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor, test } from "ember-qunit";
 
-moduleFor('controller:acercade', 'Unit | Controller | acercade', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+moduleFor("controller:acercade", "Unit | Controller | acercade", {
+  needs: ["service:electron"]
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test("it exists", function(assert) {
   let controller = this.subject();
   assert.ok(controller);
 });
