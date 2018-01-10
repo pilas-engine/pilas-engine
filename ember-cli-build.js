d37b2d71ba2966434d35b9eeb118852142a663e9
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-01-10 00:34:09 -0300

    Arreglo menor.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,22 @@
 /* eslint-env node */
 "use strict";
-
 const Funnel = require("broccoli-funnel");
 const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ["vs"]
       exclude: ["vs", "manual", "api"]
     }
   });
 
   app.import("vendor/typescript.js");
   app.import("vendor/beautify.js");
-  /*
+
   var assetsExtra = new Funnel("pilas-engine/actores", {
     srcDir: "/",
     destDir: "/actores"
   });
 
-  return app.toTree(/*assetsExtra*/);
+  return app.toTree(assetsExtra);
 };
