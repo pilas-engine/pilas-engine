d720a8c8bb998e0596624a65adb9062b13c17666
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-02-03 21:07:45 -0300

    Haciendo que la pagina actual del manual quede persistente en la URL y agregando botón para abrir el manual closes #8

--- app/components/pilas-manual.js
+++ app/components/pilas-manual.js
@@ -1,6 +1,27 @@
 import Component from "@ember/component";
+import { task, timeout } from "ember-concurrency";
 
 export default Component.extend({
-  tagName: "",
-  url: "./manual/index.html"
+  classNames: ["flex1"],
+  url: "./manual/index.html",
+  currentURL: "",
+
+  observarURL: task(function*() {
+    while (true) {
+      let element = this.$("iframe").get(0);
+      let url = element.contentDocument.location.href;
+
+      if (this.get("currentURL") != url && url != "about:blank") {
+        this.set("currentURL", url);
+        this.get("cuandoCambiaURL")(url);
+      }
+
+      yield timeout(500);
+    }
+  }),
+
+  didInsertElement() {
+    this.get("observarURL").perform({});
+    this.set("iframeURL", this.get("url"));
+  }
 });
---
+++ app/controllers/manual.js
@@ -0,0 +1,16 @@
+import Controller from "@ember/controller";
+
+export default Controller.extend({
+  url: "./manual/index.html",
+  queryParams: ["url"],
+
+  actions: {
+    cuandoCambiaURL(url) {
+      this.set("url", url);
+    },
+
+    abrirEnUnaVentanaNueva() {
+      window.open(this.get("url"));
+    }
+  }
+});
