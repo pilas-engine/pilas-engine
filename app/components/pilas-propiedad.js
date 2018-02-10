b240d7f47c9922986b8343a5e919ae0dd5c11788
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-02-10 15:45:11 -0300

    Mejorando el componente pilas-propiedad, ahora se puede definir el valor mediante un input.

--- app/components/pilas-propiedad.js
+++ app/components/pilas-propiedad.js
@@ -4,6 +4,7 @@ import Ember from "ember";
 export default Component.extend({
   original_value: 0,
   intensidad: 0.01,
+  editando: false,
 
   didInsertElement() {
     let element = this.$(".cursor-resize");
@@ -50,5 +51,30 @@ export default Component.extend({
     let valorActual = this.get("objeto").get(propiedad);
 
     this.get("modificarAtributo")(propiedad, +valorActual + delta);
+  },
+
+  actions: {
+    modificar_desde_input(objeto, propiedad, valor) {
+      if (!isNaN(+valor) && isFinite(+valor)) {
+        this.get("modificarAtributo")(propiedad, +valor);
+      }
+    },
+    comenzar_a_editar() {
+      this.set("editando", true);
+
+      Ember.run.later(() => {
+        this.$("input").focus();
+        this.$("input")[0].select();
+      });
+    },
+    cuando_pierde_foco() {
+      this.set("editando", false);
+    },
+    cuando_suelta_tecla(evento) {
+      if (evento.keyCode === 13) {
+        this.set("editando", false);
+      }
+    }
+
   }
 });
