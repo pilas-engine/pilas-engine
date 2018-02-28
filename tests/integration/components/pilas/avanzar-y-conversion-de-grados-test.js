df4607e55698b1b358dfa660088734cedd1fc8fb
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-02-28 11:38:10 -0300

    Corrigiendo tests.

--- tests/integration/components/pilas/avanzar-y-conversion-de-grados-test.js
+++ tests/integration/components/pilas/avanzar-y-conversion-de-grados-test.js
@@ -16,17 +16,17 @@ test("it renders", function(assert) {
 
     assert.equal(actor.y, 0, "Está en la posición inicial.");
 
-    actor.avanzar();
+    actor.avanzar(90);
     assert.equal(actor.y, 1, "Avanzó un solo pixel.");
 
     actor.avanzar(0, 50);
     assert.equal(actor.y, 1, "Mantiene y=1.");
     assert.equal(actor.x, 50, "Avanzó 50 pixeles porque se movió en el ángulo 0.");
 
-    assert.equal(pilas.utilides.convertir_radianes_a_angulos(0), 0);
-    assert.equal(pilas.utilides.convertir_radianes_a_angulos(Math.PI), 180);
-    assert.equal(pilas.utilides.convertir_angulo_a_radianes(90), Math.PI / 2);
-    assert.equal(pilas.utilides.convertir_angulo_a_radianes(180), Math.PI);
+    assert.equal(pilas.utilidades.convertir_radianes_a_angulos(0), 0);
+    assert.equal(pilas.utilidades.convertir_radianes_a_angulos(Math.PI), 180);
+    assert.equal(pilas.utilidades.convertir_angulo_a_radianes(90), Math.PI / 2);
+    assert.equal(pilas.utilidades.convertir_angulo_a_radianes(180), Math.PI);
   });
 
   this.set("cuandoTerminaLaEspera", pilas => {
