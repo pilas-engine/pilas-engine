51f07d84c429cf960a3c0a8067ab392e0424c762
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-03 19:22:36 -0300

    Terminando de renombrar pilas-phaser-experimental a pilas-canvas y quitando instalación de svgexport en circle-ci (aparentemente no se usa).

--- tests/integration/components/pilas-canvas-test.js
+++ tests/integration/components/pilas-canvas-test.js
@@ -1,11 +1,11 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('pilas-phaser-experimental', 'Integration | Component | pilas phaser experimental', {
+moduleForComponent("pilas-canvas", "Integration | Component | pilas canvas", {
   integration: true
 });
 
-test('it renders', function(assert) {
+test("it renders", function(assert) {
   this.render(hbs`{{pilas-canvas}}`);
   assert.ok(this.$().text());
 });
