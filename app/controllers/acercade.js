58a1a4091bae0849f059257b1156879420d93f1d
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-04-27 07:50:05 -0300

    Completando la sección "acerca de..."

--- app/controllers/acercade.js
+++ app/controllers/acercade.js
@@ -1,12 +1,15 @@
-import { inject as service } from '@ember/service';
-import Controller from '@ember/controller';
+import { inject as service } from "@ember/service";
+import Controller from "@ember/controller";
 
 export default Controller.extend({
   electron: service(),
 
   actions: {
-    abrirSitioDePilas() {
+    abrir_el_sitio_de_pilas() {
       this.electron.abrir_en_un_navegador("http://pilas-engine.com.ar");
+    },
+    abrir_el_foro_de_pilas() {
+      this.electron.abrir_en_un_navegador("http://foro.pilas-engine.com.ar/");
     },
     abrirInspector() {
       this.electron.abrirInspector();
