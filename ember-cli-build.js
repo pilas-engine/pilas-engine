40be019802fb01f7b5ab298ba15d414c11620e0f
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-04-27 18:35:25 -0300

    Reparando acceso a fuentes bitmap sobre surge.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ["vs", "manual", "api", "imagenes"]
-    },
+      exclude: ["vs", "manual", "api", "imagenes", "fuentes", "sonidos"]
+    }
   });
 
   app.import("vendor/beautify.js");
