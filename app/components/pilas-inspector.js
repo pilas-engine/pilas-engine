fb10f4c3bcde8c14c8040b23a2c003393758373b
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-04-30 17:17:45 -0300

    Reparando tests.

--- app/components/pilas-inspector.js
+++ app/components/pilas-inspector.js
@@ -1,4 +1,3 @@
-import { computed } from "@ember/object";
 import Component from "@ember/component";
 
 export default Component.extend({
--- tests/integration/components/pilas-panel-de-escenas-test.js
+++ tests/integration/components/pilas-panel-de-escenas-test.js
@@ -1,17 +1,15 @@
-import { module, test } from 'qunit';
+import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render, find } from '@ember/test-helpers';
+import { render, find } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 
 module("Integration | Component | pilas panel de escenas", function(hooks) {
   setupRenderingTest(hooks);
 
   test("it renders", async function(assert) {
-    await render(hbs`{{pilas-panel-de-escenas}}`);
-    assert.equal(
-      find('*').textContent
-        .trim(),
-      "sin escenas"
-    );
+    this.set("cuandoSelecciona", () => {});
+    await render(hbs`{{pilas-panel-de-escenas cuandoSelecciona=cuandoSelecciona}}`);
+    let elemento = find("*");
+    assert.ok(elemento.textContent.includes("sin escenas"));
   });
 });
