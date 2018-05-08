5db85ec192a6372437a6af96b8fd29464d545dc6
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-05-08 19:38:39 -0300

    Realizando prueba de postMessage sobre https

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,7 +1,8 @@
 var HOST = "file://";
 
-if (window.location.origin) {
-  HOST = window.location.origin;
+if (window.location.host) {
+  HOST = "http://" + window.location.host;
+  console.log({ HOST, origin: window.location.origin });
 }
 
 export default { HOST };
