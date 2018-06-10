bb56c29466a8362c0e97619e8804219c3ceaecf5
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-06-10 01:00:47 -0300

    Mejorando los botones de la página inicial, ahora se ven completos en mobile.

--- app/components/pilas-boton-principal.js
+++ app/components/pilas-boton-principal.js
@@ -3,7 +3,7 @@ import Component from "@ember/component";
 export default Component.extend({
   tagName: "",
   class: `
-    ba link br2 pa3 w-30
+    ba link br2 pa3 w-30-ns w-100
     verdana f6 no-underline black
     bg-animate hover-bg-black-10
     b--black-10 pointer
