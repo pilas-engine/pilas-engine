e5393f54e268f59380956e3c7b3c5f74e3155f85
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-06-13 19:17:11 -0300

    Actualizando ember-cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,13 @@ module.exports = {
   overrides: [
     // node files
     {
-      files: ["ember-cli-build.js", "testem.js", "config/**/*.js", "lib/*/index.js"],
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
       parserOptions: {
         sourceType: "script",
         ecmaVersion: 2015
