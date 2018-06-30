dc3359e47e6f65c7fda6255e66e146752924d8cb
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-06-30 15:14:43 -0300

    Agregando iconos al panel de actores.

--- app/components/pilas-icono-de-actor.js
+++ app/components/pilas-icono-de-actor.js
@@ -1,5 +1,11 @@
 import Component from "@ember/component";
 
 export default Component.extend({
-  tagName: ""
+  tagName: "",
+  style: Ember.computed("actor", function() {
+    let actor = this.get("actor");
+    let rootURL = this.rootURL;
+    let url = `url('${rootURL}iconos_actores/${actor.imagen}.png')`;
+    return new Ember.String.htmlSafe(`background-image: ${url}`);
+  })
 });
