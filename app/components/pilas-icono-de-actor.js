1e608bcbf945df8fe77d558de8df63bb544a03ef
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-07-01 14:31:57 -0300

    Corrigiendo tests.

--- app/components/pilas-icono-de-actor.js
+++ app/components/pilas-icono-de-actor.js
@@ -1,11 +1,13 @@
 import Component from "@ember/component";
+import { htmlSafe } from "@ember/string";
+import { computed } from "@ember/object";
 
 export default Component.extend({
   tagName: "",
-  style: Ember.computed("actor", function() {
+  style: computed("actor", function() {
     let actor = this.get("actor");
     let rootURL = this.rootURL;
     let url = `url('${rootURL}iconos_actores/${actor.imagen}.png')`;
-    return new Ember.String.htmlSafe(`background-image: ${url}`);
+    return new htmlSafe(`background-image: ${url}`);
   })
 });
--- tests/integration/components/pilas-icono-de-actor-test.js
+++ tests/integration/components/pilas-icono-de-actor-test.js
@@ -1,26 +1,14 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module('Integration | Component | pilas-icono-de-actor', function(hooks) {
+module("Integration | Component | pilas-icono-de-actor", function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{pilas-icono-de-actor}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#pilas-icono-de-actor}}
-        template block text
-      {{/pilas-icono-de-actor}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test("it renders", async function(assert) {
+    this.set("actor", { imagen: "" });
+    await render(hbs`{{pilas-icono-de-actor actor=actor}}`);
+    assert.equal(this.element.textContent.trim(), "");
   });
 });
