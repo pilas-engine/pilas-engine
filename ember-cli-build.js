e51b003bf54f221a94a07089a4ec24ef7f910152
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-07-03 18:12:48 -0300

    Evitando que los iconos de actores se quiten del cache.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ["vs", "manual", "api", "imagenes", "fuentes", "sonidos"]
+      exclude: ["vs", "manual", "api", "imagenes", "fuentes", "iconos_actores", "sonidos"]
     }
   });
 
