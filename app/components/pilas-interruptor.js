fa9d516fc37ae1f8dba7a096d3bb3be55f3329fd
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-09-11 21:31:43 -0300

    Mejorando iconos de los paneles.

--- app/components/pilas-interruptor.js
+++ app/components/pilas-interruptor.js
@@ -1,7 +1,20 @@
 import Component from "@ember/component";
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: "",
+  resaltar: true,
+  debe_resaltar_el_fondo: computed('resaltar', 'variable', function() {
+    if (this.resaltar) {
+      return this.variable;
+    }
+  }),
+  icono_on: computed('icono', function() {
+    return `${this.icono}-on`;
+  }),
+  icono_off: computed('icono', function() {
+    return `${this.icono}-off`;
+  }),
   actions: {
     alternar() {
       // Solo si se especifica una acción, intenta respetar
