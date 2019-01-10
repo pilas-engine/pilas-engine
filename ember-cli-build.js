1be512cf61467d1b10e1a409c242e1d4dcb8cee2
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-01-09 23:57:18 -0300

    Reparando editor que fallaba al no poder importar monaco-vim

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,16 @@ const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ["vs", "manual", "api", "imagenes", "fuentes", "iconos_actores", "sonidos"]
+      exclude: [
+        "vs",
+        "manual",
+        "api",
+        "imagenes",
+        "fuentes",
+        "iconos_actores",
+        "sonidos",
+        "monaco-vim"
+      ]
     }
   });
 
