2dbe6292f313e71b1e28fb2221bfb55d16d5bc4c
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-01-12 13:47:34 -0300

    Haciendo que los puntos que dibuja el modo historia se vean con difuminado de los últimos 7 segundos.

--- pilas-engine/historia.ts
+++ pilas-engine/historia.ts
@@ -19,14 +19,27 @@ class Historia {
   }
 
   dibujar_puntos_de_las_posiciones_recorridas(graphics) {
-    this.fotos.map(historia => {
-      historia.actores.map(entidad => {
-        let { x, y } = this.pilas.utilidades.convertir_coordenada_de_pilas_a_phaser(entidad.x, entidad.y);
+    let cantidad = 60 * 7;
 
-        graphics.fillStyle(entidad.id_color, 1);
+    let historia_reciente = this.fotos.slice(-cantidad);
+    let cantidad_total = historia_reciente.length;
+
+    for (let i = 0; i < cantidad_total; i++) {
+      let historia = historia_reciente[i];
+
+      historia.actores.map(entidad => {
+        let {
+          x,
+          y
+        } = this.pilas.utilidades.convertir_coordenada_de_pilas_a_phaser(
+          entidad.x,
+          entidad.y
+        );
+
+        graphics.fillStyle(entidad.id_color, i / cantidad_total);
         graphics.fillRect(x, y, 2, 2);
       });
-    });
+    }
   }
 
   obtener_cantidad_de_posiciones() {
