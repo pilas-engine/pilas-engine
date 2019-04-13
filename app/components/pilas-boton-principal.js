ea56ba06b5544f766e2b9f2bdb08f272786f1b56
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-04-13 18:28:45 -0300

    Mejorando botones de la pantalla principal y haciendo que los modales tomen en cuenta el tema seleccionado.

--- app/components/pilas-boton-principal.js
+++ app/components/pilas-boton-principal.js
@@ -5,12 +5,12 @@ export default Component.extend({
   class: `
     ba link br2 pa3 w-30-ns w-100
     verdana f6 no-underline black
-    bg-animate hover-bg-black-10
     b--black-10 pointer
     ma2
     bg-primario
     truncate
     unselectable
     undraggable
+    boton
   `
 });
