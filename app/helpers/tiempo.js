6e25ee60cbcae532f24d816e5134d79984f2addd
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-05-02 20:32:02 -0300

    Modificando el indicador de tiempo.

--- app/helpers/tiempo.js
+++ app/helpers/tiempo.js
@@ -1,7 +1,12 @@
-import { helper } from '@ember/component/helper';
+import { helper } from "@ember/component/helper";
 
 export function tiempo(params) {
-  return new Date(params * 1000).toISOString().substr(11, 8)
+  let tiempo = new Date(params * 1000)
+    .toISOString()
+    .substr(11, 5)
+    .split(":");
+
+  return `${tiempo[0]} min ${tiempo[1]} seg`;
 }
 
 export default helper(tiempo);
