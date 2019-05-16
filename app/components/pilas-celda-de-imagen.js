f407a32105934f2719f8a41c6cf2f39f999bce76
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-05-15 23:54:12 -0300

    Añadiendo controlador de filtro.

--- app/components/pilas-celda-de-imagen.js
+++ app/components/pilas-celda-de-imagen.js
@@ -1,5 +1,15 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-  tagName: ""
+  tagName: "",
+  debeMostrar: computed("filtro", "imagen.nombre", function() {
+    let nombre = this.get("imagen.nombre");
+
+    if (this.filtro) {
+      return nombre.indexOf(this.filtro) > -1;
+    } else {
+      return true;
+    }
+  })
 });
