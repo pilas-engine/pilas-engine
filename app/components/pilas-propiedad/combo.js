97196090ef00c7759db3818585c36af83502ceea
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-06-08 02:18:58 -0300

    Corrigiendo selector.

--- app/components/pilas-propiedad/combo.js
+++ app/components/pilas-propiedad/combo.js
@@ -3,8 +3,7 @@ import Component from "@ember/component";
 export default Component.extend({
   actions: {
     al_seleccionar(valor) {
-      console.log(valor);
-      this.modificarAtributo(this.get("propiedad.propiedad"), valor.valor);
+      this.modificarAtributo(this.get("propiedad.propiedad"), valor);
     }
   }
 });
