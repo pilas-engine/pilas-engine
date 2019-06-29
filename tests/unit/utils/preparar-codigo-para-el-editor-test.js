52e4ee08d3af8ed1f7fe91a1f72b957871f60eee
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-06-29 02:59:26 -0300

    corrigiendo tests.

--- tests/unit/utils/preparar-codigo-para-el-editor-test.js
+++ tests/unit/utils/preparar-codigo-para-el-editor-test.js
@@ -1,11 +1,19 @@
-import prepararCodigoParaElEditor from 'pilas-engine/utils/preparar-codigo-para-el-editor';
-import { module, test } from 'qunit';
+import prepararCodigoParaElEditor from "pilas-engine/utils/preparar-codigo-para-el-editor";
+import { module, test } from "qunit";
 
-module('Unit | Utility | preparar_codigo_para_el_editor', function() {
-
+module("Unit | Utility | preparar_codigo_para_el_editor", function() {
   // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = prepararCodigoParaElEditor();
-    assert.ok(result);
+  test("it works", function(assert) {
+    let result = prepararCodigoParaElEditor("class demo {}");
+    assert.equal(result, "// @ts-ignore\nclass demo {}");
+
+    let result2 = prepararCodigoParaElEditor(`class demo {
+    propiedades = {
+      test: 123
+    };
+    iniciar() {}
+}`);
+
+    assert.equal(result2, "// @ts-ignore\nclass demo {\n    iniciar() {}\n}");
   });
 });
