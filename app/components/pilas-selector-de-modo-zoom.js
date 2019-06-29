cb40fef804d65d4abe722e459750b9ec77e1c87f
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-06-29 02:04:30 -0300

    Mejorando layout del canvas y los botones en modo pausa cuando se reduce demasiado el tamaño de pantalla.

--- app/components/pilas-selector-de-modo-zoom.js
+++ app/components/pilas-selector-de-modo-zoom.js
@@ -2,6 +2,8 @@ import Component from "@ember/component";
 import { computed } from "@ember/object";
 
 export default Component.extend({
+  tagName: "",
+
   modoZoomReal: computed("modoZoom", function() {
     return this.modoZoom === 2;
   }),
