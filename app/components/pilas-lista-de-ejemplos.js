fad95c5d48054b8c64420231553b2409634f4f8e
Hugo Ruscitti <hugoruscitti@gmail.com>
2019-12-01 17:04:08 -0300

    Agregando desplazamiento en la lista de ejemplos.

--- app/components/pilas-lista-de-ejemplos.js
+++ app/components/pilas-lista-de-ejemplos.js
@@ -3,7 +3,7 @@ import { inject as service } from "@ember/service";
 
 export default Component.extend({
   ejemplos: service(),
-  cantidad_de_ejemplos: 17,
+  cantidad_de_ejemplos: 18,
 
   didInsertElement() {
     this.ejemplos.obtener();
