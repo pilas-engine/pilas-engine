a04cd739a89c6c3bed4f684691c2ec48b747f418
Hugo Ruscitti <hugoruscitti@gmail.com>
2020-07-01 10:33:30 -0300

    Corrigiendo tests.

--- app/routes/ejemplos/ver.js
+++ app/routes/ejemplos/ver.js
@@ -1,6 +1,5 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
-import { observer } from "@ember/object";
 
 export default Route.extend({
   ejemplos: service(),
