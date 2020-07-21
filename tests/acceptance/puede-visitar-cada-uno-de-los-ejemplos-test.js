878d39a4f1d141aba64c764071c85aaa5b2b5bce
Hugo Ruscitti <hugoruscitti@gmail.com>
2020-07-21 02:53:22 -0300

    Creando tests para poder probar que se pueden visitar todos los ejemplos.

---
+++ tests/acceptance/puede-visitar-cada-uno-de-los-ejemplos-test.js
@@ -0,0 +1,36 @@
+import { currentURL, visit } from "@ember/test-helpers";
+import { module, test } from "qunit";
+import { setupApplicationTest } from "ember-qunit";
+import pulsar from "pilas-engine/tests/helpers/pulsar";
+import esperar from "pilas-engine/tests/helpers/esperar";
+//import esperarElemento from "pilas-engine/tests/helpers/esperar-elemento";
+
+module("Acceptance | puede visitar cada uno de los ejemplos", function(hooks) {
+  setupApplicationTest(hooks);
+
+  const PAUSA = 3;
+
+  test("puede visitar cada uno de los ejemplos", async function(assert) {
+    await visit("/");
+
+    assert.dom("#pilas-logo").exists();
+    assert.equal(currentURL(), "/", "Está en la ruta principal");
+
+    await pulsar("Explorar ejemplos");
+    assert.equal(currentURL(), "/ejemplos", "Ingresó en la sección de ejemplos");
+
+    await esperar(PAUSA);
+
+    let items = document.querySelectorAll("a.ejemplo");
+
+    assert.equal(1, 1, `Prueba los ${items.length} ejemplos:`);
+
+    for (let i = 0; i < items.length; i++) {
+      let link = items[i].getAttribute("href");
+
+      assert.equal(1, 1, `Probando ejemplo ${i}/${items.length}`);
+      await visit(link);
+      await esperar(PAUSA);
+    }
+  });
+});
