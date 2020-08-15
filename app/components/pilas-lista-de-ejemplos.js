6bbfd2bb6fb18daab803a0d440ae367e89cdd72a
Hugo Ruscitti <hugoruscitti@gmail.com>
2020-08-15 11:27:33 -0300

    Mejorando listado de ejemplos, ahora aparecen centrados y sin ocupar el 100% del ancho de ventana.

--- app/components/pilas-lista-de-ejemplos.js
+++ app/components/pilas-lista-de-ejemplos.js
@@ -2,6 +2,7 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  classNames: ['tc'],
   ejemplos: service(),
   cantidad_de_ejemplos: 35,
 
