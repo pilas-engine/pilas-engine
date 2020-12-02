3af45bd9c551bbd162be2b1b00b31294687291fc
Hugo Ruscitti <hugoruscitti@gmail.com>
2020-12-02 20:27:02 -0300

    Aumentando el límites de sonidos de 200 a 400 kb

--- app/components/pilas-boton-importar-sonido.js
+++ app/components/pilas-boton-importar-sonido.js
@@ -10,12 +10,12 @@ export default Component.extend({
 
     procesar_sonido_seleccionado(event) {
       let encontrado = [...event.target.files].filter(archivo => {
-        return archivo.size / 1024 > 200;
+        return archivo.size / 1024 > 400; // 400kb
       });
 
       if (encontrado.length > 0) {
         let archivo = encontrado[0];
-        alert(`No se puede incorporar el archivo "${archivo.name}" porque excede los 200 kb. Te recomendamos optimizar este archivo con un software como audacity para que pilas pueda procesarlo.`);
+        alert(`No se puede incorporar el archivo "${archivo.name}" porque excede los 400kb. Te recomendamos optimizar este archivo con un software como audacity para que pilas pueda procesarlo.`);
         return;
       }
 
