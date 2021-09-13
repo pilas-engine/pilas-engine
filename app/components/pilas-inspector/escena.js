4b7b4db0adcf275d31027a6c3328a04d81cdc376
Hugo Ruscitti <hugoruscitti@gmail.com>
2021-09-13 10:28:36 -0300

    Quitando el filtro "fondo" al seleccionar una imagen de fondo para la escena.

--- app/components/pilas-inspector/escena.js
+++ app/components/pilas-inspector/escena.js
@@ -52,7 +52,7 @@ export default Component.extend({
       {
         tipo: "imagen",
         propiedad: "fondo",
-        filtroPropuesto: "fondo",
+        filtroPropuesto: "",
         etiqueta: "stage.background"
       },
       {
