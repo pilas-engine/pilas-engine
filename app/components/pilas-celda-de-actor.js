0d4c5cf36745f4f1614606c0f5ae302d2b78dcd7
Hugo Ruscitti <hugoruscitti@gmail.com>
2022-01-01 17:00:30 -0300

    Creando un filtro en la pantalla de creación de actores.

--- app/components/pilas-celda-de-actor.js
+++ app/components/pilas-celda-de-actor.js
@@ -1,5 +1,14 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-  tagName: ""
+  tagName: "",
+
+  debeOcultarPorFiltro: computed("filtro", function() {
+    return ! this.actor.nombre.includes(this.filtro);
+  }),
+
+  nombreSeparadoEnPalabras: computed("actor.nombre", function() {
+    return this.actor.nombre.replace(/_/g, " ");
+  })
 });
